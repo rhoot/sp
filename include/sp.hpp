6267300a471060a0007413d19be823bc3f8efc5b
Johan Sköld <git@johan.skold.cc>
2018-05-09 15:55:59 -0700

    Adding missing pragma once

--- include/sp.hpp
+++ include/sp.hpp
@@ -9,6 +9,8 @@
 // You should have received a copy of the CC0 Public Domain Dedication along
 // with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+#pragma once
+
 #include <cmath> // NAN, INFINITY
 #include <cstddef> // std::nullptr_t
 #include <cstdint> // int32_t, uint64_t
